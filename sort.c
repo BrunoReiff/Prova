ab399a6448eeae631bca9df951a7c8c08426f328
Bruno <81494923+BrunoReiff@users.noreply.github.com>
2025-06-18 15:22:05 -0300

    sort.c

--- sort.c
+++ sort.c
@@ -3,30 +3,26 @@
 #include <time.h>
 
 int main() {
-    int vetor[10], i, j, temp;
-
-    printf("Vamos gerar os elementos do vetor:\n");
-    for (int i = 0; i < 10; i++) {
-        vetor[i] = rand()%100;
-        printf("%d " ,vetor[i]);
+    int vet[10], aux;
+    int tamanho = sinzeof(vet) / sizeof(vet[0]);
+    
+    for (int i = 0; i < tamanho; i++) {
+        vet[i] = rand()%100;
+        printf("%d " ,vet[i]);
     }    
     
-    //Sort
-    for (i = 0; i < 10; i++) {
-        for (j = i; j < 10; j++) {
-            if (vetor[i] > vetor[j + 1]) {
-                // Troca os elementos
-                temp = vetor[i];
-                vetor[i] = vetor[j + 1];
-                vetor[j + 1] = temp;
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = i; j < tamanho; j++) {
+            if (vet[i] > vet[j + 1]) {
+                aux = vet[i];
+                vet[i] = vet[j + 1];
+                vet[j + 1] = aux;
             }
         }
     }
 
-    // Exibindo o vetor ordenado
-    printf("\n\nVetor ordenado:\n");
-    for (i = 0; i < 10; i++) {
-        printf("%d ", vetor[i]);
+    for (i = 0; i < tamanho; i++) {
+        printf("%d ", vet[i]);
     }
     printf("\n");
 
